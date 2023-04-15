df4a8830766abb123be6710a034599622a999828
fritzprix <innocentevil0914@gmail.com>
2023-04-15 09:26:09 +0900

    updated import

--- langchain/memory/__init__.py
+++ langchain/memory/__init__.py
@@ -4,7 +4,8 @@ from langchain.memory.buffer import (
 )
 from langchain.memory.buffer_window import ConversationBufferWindowMemory
 from langchain.memory.chat_message_histories.dynamodb import DynamoDBChatMessageHistory
-from langchain.memory.chat_message_histories.in_memory import ChatMessageHistory, BackendSupportedMessageHistory
+from langchain.memory.chat_message_histories.in_memory import ChatMessageHistory
+from langchain.memory.chat_message_histories.be_supported import BackendSupportedMessageHistory
 from langchain.memory.chat_message_histories.postgres import PostgresChatMessageHistory
 from langchain.memory.chat_message_histories.redis import RedisChatMessageHistory
 from langchain.memory.combined import CombinedMemory
